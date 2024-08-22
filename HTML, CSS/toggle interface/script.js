302169e7b9b4dd7754986dae250fa792dcb100d6
essay104 <alexis104@naver.com>
2024-08-22 23:39:28 +0900

    240822

--- HTML, CSS/toggle interface/script.js
+++ HTML, CSS/toggle interface/script.js
@@ -1,9 +1,14 @@
 const header = document.querySelector("header");
 const menu = document.querySelectorAll(".menu");
+const headerTop = document.querySelector(".header-top");
 const dropDown = document.querySelector(".header-bottom");
 const buttons = document.querySelectorAll(".buttons");
 const tabItems = document.querySelector(".items");
 const h1 = tabItems.querySelectorAll("h1");
+
+const state = {
+  color: "#ffd700",
+};
 
 menu.forEach((it) => {
   it.addEventListener("mouseover", () => {
@@ -20,9 +25,18 @@ buttons.forEach((button, idx) => {
     tabItems.style.left = `-${idx * 100}%`;
 
     buttons.forEach((button) => button.classList.remove("active"));
-    button.classList.add("acitve");
+    button.classList.add("active");
 
     h1.forEach((title) => title.classList.remove("active"));
     h1[idx].classList.add("active");
+
+    const selectedColor = button.getAttribute("interface-color");
+
+    state.color = selectedColor;
+    console.log(state.color);
+
+    headerTop.style.borderBottom = `5px solid ${state.color}`;
+    menu.forEach((menuItem) => (menuItem.style.color = state.color));
+    dropDown.style.backgroundColor = state.color;
   });
 });
